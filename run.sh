e48a75f71fbc592a53ab001288241ccc3c834c51
sawinjer <polisukandrij68@gmail.com>
2026-04-28 18:10:33 +0300

    docker

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-# infisical run --env dev --projectId d78f7abc-4b68-42d0-a613-5a5e48a7a1a0 -c 'printenv'
-# infisical run --env dev --projectId d78f7abc-4b68-42d0-a613-5a5e48a7a1a0 -c 'echo $SECRET'
 infisical run --env dev --projectId d78f7abc-4b68-42d0-a613-5a5e48a7a1a0 -c 'bun dev'
